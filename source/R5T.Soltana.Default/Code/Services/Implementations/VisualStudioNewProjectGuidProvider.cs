8e8d2e765834bfc3f8b8db92588b3e53a384b7b5
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:22:54 -0800

    Added IVisualStudioNewProjectGuidProvider service definition and default implementation.

---
+++ source/R5T.Soltana.Default/Code/Services/Implementations/VisualStudioNewProjectGuidProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace R5T.Soltana.Default
+{
+    public class VisualStudioNewProjectGuidProvider : IVisualStudioNewProjectGuidProvider
+    {
+        public Guid GetNewVisualStudioProjectGuid()
+        {
+            var newProjectGuid = Guid.NewGuid();
+            return newProjectGuid;
+        }
+    }
+}
