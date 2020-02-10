760e43a0940c83e65679171dc5d0b1e1fc592fdf
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:23:33 -0800

    Added ISolutionFolderPathOperator service definition and default implementation.

---
+++ source/R5T.Soltana.Default/Code/Services/Definitions/ISolutionFolderPathOperator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace R5T.Soltana.Default
+{
+    public interface ISolutionFolderPathOperator
+    {
+        IEnumerable<string> GetSolutionFolderPathParts(string solutionFolderPath);
+
+        string GetSolutionFolderPath(IEnumerable<string> solutionFolderPathParts);
+    }
+}
---
+++ source/R5T.Soltana.Default/Code/Services/Implementations/SolutionFolderPathOperator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Magyar.Extensions;
+
+
+namespace R5T.Soltana.Default
+{
+    public class SolutionFolderPathOperator : ISolutionFolderPathOperator
+    {
+        private ISolutionFolderPathConventions SolutionFolderPathConventions { get; }
+
+
+        public SolutionFolderPathOperator(ISolutionFolderPathConventions solutionFolderPathConventions)
+        {
+            this.SolutionFolderPathConventions = solutionFolderPathConventions;
+        }
+
+        public string GetSolutionFolderPath(IEnumerable<string> solutionFolderPathParts)
+        {
+            var solutionFolderPathSeparator = this.SolutionFolderPathConventions.GetSolutionFolderSeparator();
+
+            var solutionFolderPath = String.Join(solutionFolderPathSeparator, solutionFolderPathParts);
+            return solutionFolderPath;
+        }
+
+        public IEnumerable<string> GetSolutionFolderPathParts(string solutionFolderPath)
+        {
+            var solutionFolderPathSeparator = this.SolutionFolderPathConventions.GetSolutionFolderSeparator();
+
+            var solutionFolderPathParts = solutionFolderPath.Split(solutionFolderPathSeparator);
+            return solutionFolderPathParts;
+        }
+    }
+}
