c9650460d7b764e6c7847a79489fb05fce23491f
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:22:11 -0800

    Added SolutionFolderPathConventions service definition and default implementation.

---
+++ source/R5T.Soltana.Default/Code/Services/Definitions/ISolutionFolderPathConventions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace R5T.Soltana.Default
+{
+    public interface ISolutionFolderPathConventions
+    {
+        string GetSolutionFolderSeparator();
+    }
+}
---
+++ source/R5T.Soltana.Default/Code/Services/Implementations/SolutionFolderPathConventions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Soltana.Default
+{
+    public class SolutionFolderPathConventions : ISolutionFolderPathConventions
+    {
+        public const string SolutionFolderPathSeparator = "/";
+
+
+        public string GetSolutionFolderSeparator()
+        {
+            return SolutionFolderPathConventions.SolutionFolderPathSeparator;
+        }
+    }
+}
