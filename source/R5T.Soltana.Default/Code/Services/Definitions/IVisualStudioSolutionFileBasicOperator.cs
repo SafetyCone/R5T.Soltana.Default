01ee71034010472f88c7086d1b5728457884a629
MinexAutomation <minex.automation@gmail.com>
2020-02-10 11:24:05 -0800

    Added IVisualStudioSolutionFileBasicOperator service definition and default implementation.

---
+++ source/R5T.Soltana.Default/Code/Services/Definitions/IVisualStudioSolutionFileBasicOperator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using R5T.Cambridge.Types;
+
+
+namespace R5T.Soltana.Default
+{
+    public interface IVisualStudioSolutionFileBasicOperator
+    {
+        void AddSolutionFolder(SolutionFile solutionFile, string solutionFolder);
+
+        IEnumerable<SolutionFileProjectReference> GetSolutionFolders(SolutionFile solutionFile);
+
+        IEnumerable<SolutionFileProjectReference> GetRootSolutionFolders(SolutionFile solutionFile);
+    }
+}
---
+++ source/R5T.Soltana.Default/Code/Services/Implementations/VisualStudioSolutionFileBasicOperator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using R5T.Cambridge.Types;
+
+
+namespace R5T.Soltana.Default
+{
+    public class VisualStudioSolutionFileBasicOperator : IVisualStudioSolutionFileBasicOperator
+    {
+        public void AddSolutionFolder(SolutionFile solutionFile, string solutionFolder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SolutionFileProjectReference> GetRootSolutionFolders(SolutionFile solutionFile)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SolutionFileProjectReference> GetSolutionFolders(SolutionFile solutionFile)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
